adcf82ef2666459bfbe27ca5c2e9b80cd9739fd7
cheshir <nyahamaha82@gmail.com>
2021-07-21 10:00:33 +0300

    урок 7 - задание 3

---
+++ homework7-3.py
@@ -0,0 +1,23 @@
+class Cell:
+    def __init__(self, cell):
+        self.cell = cell
+
+    def __str__(self):
+        pass
+
+    def __add__(self, other):
+        pass
+    def make_order(self, raw):
+        pass
+
+
+cell_1 = Cell(12)
+cell_2 = Cell(13)
+print(cell_1 + cell_2)
+print(cell_2.make_order(3))
+
+***
+***
+***
+***
+*
