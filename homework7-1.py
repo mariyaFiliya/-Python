b0c503152358706ca251eadb538dfe003b70263e
mariyaFiliya <86361200+mariyaFiliya@users.noreply.github.com>
2021-07-22 10:16:37 +0300

    Update homework7-1.py

--- homework7-1.py
+++ homework7-1.py
@@ -3,13 +3,18 @@ class Matrix:
         self.matrix = matrix
 
     def __str__(self):
-        pass
+        return '\n'.join('\t'.join([f'{item:3}' for item in line]) for line in self.matrix)
 
     def __add__(self, other):
-        pass
+        try:
+            n = [[int(self.matrix[line][item]) + int(other.matrix[line][item]) for item in range(len(self.matrix[line]))] for line in range(len(self.matrix))]
+            return Matrix(n)
+        except IndexError:
+            return "err"
 
 
-matrix_1 = Matrix([[1, 2], [3, 4], [5, 6]])
-matrix_2 = Matrix([[7, 8], [9, 10], [11, 12]])
+matrix_1 = Matrix([[2, 5, 7], [1, 1, 6], [3, 12, 7]])
+matrix_2 = Matrix([[0, 23, 5], [2, 5, 3], [5, 4, 8]])
 print(matrix_1)
+print(matrix_2)
 print(matrix_1 + matrix_2)
