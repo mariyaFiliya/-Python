8893dee133ef4a3bf36326f4b91e5089ee37e3b3
mariyaFiliya <86361200+mariyaFiliya@users.noreply.github.com>
2021-07-22 10:18:20 +0300

    Update homework7-3.py

--- homework7-3.py
+++ homework7-3.py
@@ -2,22 +2,24 @@ class Cell:
     def __init__(self, cell):
         self.cell = cell
 
+    def make_order(self, raw):
+        return '\n'.join(["○" * raw for _ in range(self.cell // raw)]) + '\n' + "○" * (self.cell % raw)
+
     def __str__(self):
-        pass
+        return f'{self.cell}'
 
     def __add__(self, other):
-        pass
-    def make_order(self, raw):
-        pass
+        print('сумма клеток равна: ')
+        return Cell(self.cell + other.cell)
+
+    def __sub__(self, other):
+        print('разница клеток равна: ')
+        return Cell(
+            self.cell - other.cell) if self.cell - other.cell > 0 else 'вычитание невозможно, получается отрицательное значение'
 
 
-cell_1 = Cell(12)
-cell_2 = Cell(13)
+
+cell_1 = Cell(15)
+cell_2 = Cell(4)
 print(cell_1 + cell_2)
-print(cell_2.make_order(3))
-
-***
-***
-***
-***
-*
+print(cell_1.make_order(5))
